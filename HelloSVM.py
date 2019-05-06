5f323b598ad49d81fe50a99c84125330df7500c1
yallenh <yanhsianghuang@gmail.com>
2019-05-06 12:09:19 -0700

    final version

---
+++ HelloSVM.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from matplotlib import style
+style.use("ggplot")
+
+
+# In[3]:
+
+
+x = [1,5,1.5,8,1,9]
+y = [2,8,1.8,8,0.6,11]
+
+plt.scatter(x,y)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
